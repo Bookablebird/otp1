4962fdfc34213d1db51dc0157839cea27dc2237f
valttejh <valtteri.hemmila@metropolia.fi>
2019-09-12 11:44:02 +0300

    maven

---
+++ varastp/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $otp.varastp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
